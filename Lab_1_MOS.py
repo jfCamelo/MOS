77371dfae7de78a7cf9525d045be6116f8f98e19
jfCamelo <j.camelob@uniandes.edu.co>
2024-08-15 13:16:26 -0500

    coso

--- Lab_1_MOS.py
+++ Lab_1_MOS.py
@@ -7,12 +7,12 @@ from pyomo.opt import SolverFactory
 Model = ConcreteModel()
 
 # Data de entrada
-trabajadores=4
+totales=11
 
-p=RangeSet(1, trabajadores)
+p=RangeSet(1, totales)
 
-puntos_historia={1:5, 2:3, 3:13, 4:1, 5:21, 6:2, 7:2, 8:5, 9:8, 10:13, 12:21}
-puntos_prioridada={1:7, 2:5, 3:6, 4:3, 5:1, 6:4, 7:6, 8:6, 9:2, 10:7, 12:6}
+puntos_historia={1:5, 2:3, 3:13, 4:1, 5:21, 6:2, 7:2, 8:5, 9:8, 10:13, 11:21}
+puntos_prioridada={1:7, 2:5, 3:6, 4:3, 5:1, 6:4, 7:6, 8:6, 9:2, 10:7, 11:6}
 
 # Variable de decisión
 Model.x = Var(p, domain=Binary)
@@ -21,7 +21,7 @@ Model.x = Var(p, domain=Binary)
 Model.obj = Objective(expr = sum(Model.x[i]*puntos_prioridada[i] for i in p), sense=maximize)
 
 # Restricciones
-Model.res1 = Constraint(expr = sum(Model.x[i]*puntos_historia[i] for i in p) <= 13)
+Model.res1 = Constraint(expr = sum(Model.x[i]*puntos_historia[i] for i in p) <= 52)
 
 # Especificación del solver
 SolverFactory('glpk').solve(Model)
