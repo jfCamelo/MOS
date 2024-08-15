3d88360bf8ef713f18f03e45010e58206c520379
jfCamelo <j.camelob@uniandes.edu.co>
2024-08-15 13:30:27 -0500

    arreglo

--- Lab_1_P_2.py
+++ Lab_1_P_2.py
@@ -18,10 +18,10 @@ ganancia={1:50, 2:60, 3:40, 4:70, 5:30}
 horas={1:4, 2:5, 3:3, 4:6, 5:2}
 
 # Variable de decisión
-Model.x = Var(p, domain=Binary)
+Model.x = Var(p,q,domain=Binary)
 
 # Función objetivo
-Model.obj = Objective(expr = sum(Model.x[i]*ganancia[i] for i in p), sense=maximize)
+Model.obj = Objective(expr = sum(Model.x[i,j]*ganancia[i] for i in p for j in q), sense=maximize)
 
 # Restricciones
 for j in range(1,3):
