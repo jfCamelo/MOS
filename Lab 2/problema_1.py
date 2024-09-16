7adf1894a52057be51dadcac690c35243cf2e53d
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2024-09-16 18:20:20 -0500

    ánalisis de sensibilidad realizado

--- Lab 2/problema_1.py
+++ Lab 2/problema_1.py
@@ -16,10 +16,10 @@ origen=RangeSet(1, ciudadOrigen)
 #Variable Auxiliar
 
 desde = ["Bogotá", "Medellín"]
-hasta = ["Cali", "Barranquilla", "Pasto", "Tunja", "Chía", "Manizales."]
+hasta = ["Cali", "Bquilla", "Pasto", "Tunja", "Chía", "Manizales"]
 
 #Conjunto
-demanda = {1:125,2:175,3:225, 4:250, 5:225, 6:200}
+demanda = {1:125,2:175,3:225, 4:250, 5:225, 6:200, 7:50}
 oferta = {1:550, 2:700}
 costos = {1:{1:999, 2:2.5}, 2:{1:2.5, 2:999}, 3: {1:1.6, 2:2.0}, 4:{1:1.4, 2:1.0}, 5:{1:0.8, 2:1.0}, 6:{1:1.4, 2:0.8}}
 
@@ -41,14 +41,11 @@ for i in destino:
 
 # Especificación del solver
 SolverFactory('glpk').solve(Model)
+results = SolverFactory('glpk').solve(Model)
 
 Model.display()
 
 #Análisis de sensibilidad
-
-# print("\nSensitivity Analysis")
-# print(f"Oferta desde orígen = {-Model.dual[Model.lista1]}")
-# print(f"Demanda en destino = {-Model.dual[Model.lista2]}")
 
 print("\nAnálisis de sensibilidad")
 for i, constr in enumerate(Model.lista1):
@@ -56,5 +53,15 @@ for i, constr in enumerate(Model.lista1):
 
 for i, constr in enumerate(Model.lista2):
     print(f"Demanda en destino {hasta[i]} = {-Model.dual[Model.lista2[i+1]]}")
+
+
+print("\n")
+if 'ok' in str(results.Solver.status):    
+    print("Cliente      Demanda   Enviado    Margen")
+    for i, destino_name in enumerate(hasta):
+        total_enviado = sum(Model.x[i+1, j]() for j in origen)
+        print(f"{destino_name:10s}{demanda[i+1]:10.1f}{total_enviado:10.1f}{Model.dual[Model.lista2[i+1]]:10.4f}")
+else:
+    print("No Valid Solution Found")
 
 
